9fe5e78562a600c26d9265d5d8c292ca4059544f
Rohit-998 <rohitagrawal99819@gmail.com>
2026-09-14 00:49:52 +0530

    chore: initialize Spring Boot backend skeleton with Spring Initializr

---
+++ backend/src/main/java/com/sayhii/sayhii_backend/SayhiiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sayhii.sayhii_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayhiiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayhiiBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sayhii/sayhii_backend/SayhiiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayhii.sayhii_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayhiiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
